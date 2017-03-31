312deac39bd47396ecaacf37230a84d8243e99c7
Spencer Bouse <Orangesrb@gmail.com>
2017-03-31 10:22:08 -0400

    seperated Js files

--- js/dashboard.js
+++ js/dashboard.js
@@ -19,3 +19,13 @@ $.ajax({
         // Something should happen here
 }});
 })
+$.ajax({
+	url: `http://api.openweathermap.org/data/2.5/forecast/daily?q=47353&cnt=1&units=imperial&APPID=d0458c4189cf033bf80c84d7a0d38ab0`,
+	success:(result)=>{
+		console.log(result)
+		$(".weather").html(`
+			<img src="http://openweathermap.org/img/w/${result.list[0].weather[0].icon}.png" />
+			<h2>${result.list[0].temp.max}°</h2>
+			`)
+	}
+})
